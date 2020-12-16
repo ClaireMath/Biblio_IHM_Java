a0f172b7c78c37dbb80e1012dad4ea80249f9b85
Christopher Billy <billy_christopher@hotmail.com>
2020-12-16 17:49:37 +0100

    Story#02 - Ajout d'un FileChooser + filtre text

--- src/MyWindow.java
+++ src/MyWindow.java
@@ -1,15 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
 
 import static java.awt.Color.black;
 
 public class MyWindow extends JFrame {
     public MyWindow() {
         super("Biblio.exe");
-        setSize(900, 700);
+        this.setSize(900, 700);
         JPanel panel = new JPanel();
-        setContentPane(panel);
+        this.setContentPane(panel);
         panel.setBackground(Color.lightGray);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
@@ -31,12 +37,33 @@ public class MyWindow extends JFrame {
         menuBar.add(menuA);
         menuA.setBackground(Color.gray);
 
-
-
-
-
+        menuO.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setAcceptAllFileFilterUsed(false);
+                fileChooser.addChoosableFileFilter(new MyFilter());
+                int dialog = fileChooser.showDialog(panel, "Ouvrir");
+                if (dialog == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile();
+                    System.out.println(file.getName() + " a été ouvert");
+                }
+                else {
+                    System.out.println("Action annulée");
+                }
+            }
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+        });
     }
-
-
-
 }
