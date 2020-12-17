3510c1f96a05a0a2fbd05bb9a2178ed7628ac950
Nohan75 <nohanmarielouise@gmail.com>
2020-12-17 09:46:31 +0100

    Création du tableau et ajout au panel

--- src/MyWindow.java
+++ src/MyWindow.java
@@ -1,22 +1,61 @@
 import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
 import java.awt.*;
+import java.util.Arrays;
 
 import static java.awt.Color.black;
 
 public class MyWindow extends JFrame {
     public MyWindow() {
         super("Ma bibliothèque");
-        setSize(900, 700);
+        this.setSize(900, 700);
+        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         JPanel panel = new JPanel();
-        setContentPane(panel);
+        this.setContentPane(panel);
         panel.setBackground(Color.lightGray);
         JMenuBar menuBar = new JMenuBar();
-        setJMenuBar(menuBar);
+        this.setJMenuBar(menuBar);
 
         JMenu menu = new JMenu();
         menu.setText("Fichier");
-        menu.add(menu);
+        menuBar.add(menu);
         menu.setBackground(Color.gray);
+
+        GridBagLayout monLayout = new GridBagLayout();
+        panel.setLayout(monLayout);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        // TABLEAU
+
+        Object[][] donnee = {
+                {"Harry Potter", "J.K Rowling", "Voldemor meurt !", 5, 2, 2009},
+                {"Eragon", "C. Paolini", "Un monde de dragon", 2, 2, 2000}
+        };
+        String[] entetes = {"Titre", "Auteur", "Résumé", "Colonne", "Rangée", "Parution"};
+
+        JTable tableau = new JTable(donnee, entetes);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+
+
+
+        panel.add(tableau.getTableHeader(), gbc);
+
+
+
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+
+        panel.add(tableau, gbc);
+
+
+        pack();
         
 
 
